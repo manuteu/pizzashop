74f43e98d6f7594927eaaf5f86e564909f41edce
Matheus <matheusim27@hotmail.com>
2024-08-04 15:14:59 -0300

    Initialize repository, config shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@/global.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className='flex justify-center '>
+      <Button />
+    </div>
+  )
+}
+
+export default App
