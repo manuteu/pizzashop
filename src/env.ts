ce9f058347ce9a183ae110627a14cb108ac15a43
Matheus <matheusim27@hotmail.com>
2024-09-13 22:59:06 -0300

    test

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-const envSchema = z.object({
-  MODE: z.enum(['production', 'development', 'test']),
-  VITE_API_URL: z.string(),
-  VITE_ENABLE_API_DELAY: z.string().transform((value) => value === "true"),
-});
+// const envSchema = z.object({
+//   MODE: z.enum(['production', 'development', 'test']),
+//   VITE_API_URL: z.string(),
+//   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === "true"),
+// });
 
 export const env = import.meta.env
